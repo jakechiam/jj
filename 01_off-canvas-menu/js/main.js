ba3f258cc99df6b1fd891076b734d476c102c436
Jake Chiam <jakechiam@gmail.com>
2019-08-30 17:02:02 +0800

    Make MenuButton Toggle Sidebar Menu
    CSS:
    Add body.offsite-is-open
    transform: translateX(14rem)
    
    JS:
    Add button click event listener that CSS
    toggles('offsite-is-open')
    
    [Ticket: X]

--- 01_off-canvas-menu/js/main.js
+++ 01_off-canvas-menu/js/main.js
@@ -1,1 +1,7 @@
-// Your JavaScript goes here
+// J-Edits - JavaScript goes here
+const button = document.querySelector('button')
+const body = document.body
+
+button.addEventListener('click', event => {
+  body.classList.toggle('offsite-is-open')
+})
