cb28f1840c8fd1576006f0d0dfd3f9fb947b687b
Jake Chiam <jakechiam@gmail.com>
2019-08-30 21:12:34 +0800

    Add Modal Open and Close Functionality
    CSS:
    .modal-overlay: fix Position
    .modal-overlay: define top, bottom, left, right
    
    JS:
    .modalButton: add Click event listener
    .modalCloseButton: add Click event listener
    
    [Ticket: X]

---
+++ 02_modal/js/main.js
@@ -0,0 +1,9 @@
+const modalButton = document.querySelector('.jsModalButton')
+modalButton.addEventListener('click', event => {
+  document.body.classList.add('modal-is-open')
+})
+
+const modalCloseButton = document.querySelector('.jsModalClose')
+modalCloseButton.addEventListener('click', event => {
+  document.body.classList.remove('modal-is-open')
+})
