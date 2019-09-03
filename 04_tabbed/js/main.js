1b9b7c8b3f78e0ecfc5be62c06c24b4ac10f8b85
Jake Chiam <jakechiam@gmail.com>
2019-09-03 23:45:24 +0800

    Add Tabbed Component Click-to-Activate Tab
    CSS:
    .tab.is-selected a = button background: lighter grey
    .tab a's children = pointer-events none
    .tab:not(.is-selected) .tab__icon = rgba(0.25) faded icon
    .tab-content = display none
    .tab-content.is-selected = display grid
    
    JS:
    1. component selector
    2. Create tabsArr
    3. Creates contentsArr
    4. tabsArr.forEach(tab=>add evt listener)
          a = children[0]
          href = a.getAttribute('href')
          corrContent = component.qSel(href)
          tabsArr.forEach( tab remove .is-selected )
          contents.forEach( content remove.is-selected )
          iTab.add(.is-selected)
          iCorrContent.add(.is-selected)
    
    [Ticket: 5.7 DOM Basics]

--- 04_tabbed/js/main.js
+++ 04_tabbed/js/main.js
@@ -1,2 +1,20 @@
 // START EDITING YOUR JAVASCRIPT HERE
 // ===============
+
+const component = document.querySelector('.tabbed-component')
+const tabs = Array.from(component.querySelectorAll('.tab'))
+const contents = Array.from(component.querySelectorAll('.tab-content'))
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', e => {
+    const aLink = tab.children[0]
+    const href = aLink.getAttribute('href')
+    const corrContent = component.querySelector(href)
+
+    tabs.forEach(el => { el.classList.remove('is-selected') })
+    contents.forEach(el => { el.classList.remove('is-selected') })
+
+    tab.classList.add('is-selected')
+    corrContent.classList.add('is-selected')
+  })
+})
