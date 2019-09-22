c41289e8494fe8a592681adccfbf1e074f94fe8e
Jake Chiam <jakechiam@gmail.com>
2019-09-22 17:51:21 +0800

    Add CSS Height Transition and Javascript to Calc Height
    HTML:
    • Wrap (.accordion__content>.accordion__inner)*4
    • Remove .is-open
    
    CSS:
    • accordion__header: cursor pointer
    • accordion-content: height 0, overflow hidden, transition height 0.3s ease-out
    .accordion-inner: display grid
    
    JS:
    • get accordionContent
    • get accordinInner
    • calculate clicked's accordion height
    • if accordion is open >> set height=0 in prep
    • else accordion is closed >> set height = getBoundingClientRect().height + 'px'
    • Toggle accordion.is-open
    • Set accordionContent's inline CSS = height + 'px'
    • Add optional codeblock for ONLY ONE accordion open functionality
    
    [7.12]

--- 03_accordion/js/main.js
+++ 03_accordion/js/main.js
@@ -1,7 +1,7 @@
 // Chapter 6: Event Delegation
 // Attach JUST ONE click eventListener to PARENT accordionContainer, that catches all events bubblingUp
 // On ONE click eventListener, check if event delegation bubbleUps to inner accordion-header
-//   IF yes, then get parentElement of accordion-header, and toggle its 'is-open' class
+//   IF yes, then toggle the accordion opening/closing functionality
 
 const accordionContainer = document.querySelector('.accordion-container')
 
@@ -9,6 +9,30 @@ accordionContainer.addEventListener('click', e => {
   const accordionHeader = e.target.closest('.accordion__header')
   if (accordionHeader) {
     const accordion = accordionHeader.parentElement
+    const accordionContent = accordionHeader.nextElementSibling
+    const accordionInner = accordionContent.children[0]
+
+    let height
+
+    if (accordion.classList.contains('is-open')) {
+      height = 0
+    } else {
+      height = accordionInner.getBoundingClientRect().height
+    }
+
     accordion.classList.toggle('is-open')
+    accordionContent.style.height = height + 'px'
+
+    /* J: Tweak to Enable ONLY ONE accordion tab to be open at any one time */
+    // const accordionsArr = Array.from(accordionContainer.children)
+    // accordionsArr.forEach(acc => {
+    //   if (acc === accordion) {
+    //     accordion.classList.toggle('is-open')
+    //     accordionContent.style.height = height + 'px'
+    //   } else {
+    //     acc.classList.remove('is-open')
+    //     acc.querySelector('.accordion__content').style.height = 0 + 'px'
+    //   }
+    // })
   }
 })
